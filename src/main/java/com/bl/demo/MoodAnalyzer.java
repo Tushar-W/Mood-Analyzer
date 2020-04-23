2939520b2ab3f7b94fa55c93206c27698b04d55e
Tushar-W <twankhede95@gmail.com>
2020-04-23 10:24:27 +0530

    REFACTORED:taking mood message in constructor and tested it

--- src/main/java/com/bl/demo/MoodAnalyzer.java
+++ src/main/java/com/bl/demo/MoodAnalyzer.java
@@ -1,7 +1,13 @@
 package com.bl.demo;
 
 public class MoodAnalyzer {
-    public String analyzeMood(String message){
+    public String message;
+
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    public String analyzeMood(){
         if (message.contains("I am in Sad mood"))
             return "SAD";
         else
--- src/test/java/com/bl/demo/MoodAnalyzerTest.java
+++ src/test/java/com/bl/demo/MoodAnalyzerTest.java
@@ -7,15 +7,15 @@ public class MoodAnalyzerTest {
 
     @Test
     public void givenMood_WhenBad_ShouldReturnBad() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String mood = moodAnalyzer.analyzeMood("I am in Sad mood");
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Sad mood");
+        String mood = moodAnalyzer.analyzeMood();
         Assert.assertEquals("SAD",mood);
     }
 
     @Test
     public void givenMood_WhenHappy_ShouldReturnHappy() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String mood = moodAnalyzer.analyzeMood("I am in Any mood");
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Happy mood");
+        String mood = moodAnalyzer.analyzeMood();
         Assert.assertEquals("HAPPY",mood);
     }
 
