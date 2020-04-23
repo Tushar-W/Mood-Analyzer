47f205a7315342391fd98ed8a48d5a61ee620e58
Tushar-W <twankhede95@gmail.com>
2020-04-23 10:15:48 +0530

    ADDED:tested happy mood

--- src/test/java/com/bl/demo/MoodAnalyzerTest.java
+++ src/test/java/com/bl/demo/MoodAnalyzerTest.java
@@ -5,11 +5,18 @@ import org.junit.Test;
 
 public class MoodAnalyzerTest {
 
-        @Test
-        public void givenMood_WhenBad_ShouldReturnBad() {
-            MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-            String mood = moodAnalyzer.analyzeMood("I am in Sad mood");
-            Assert.assertEquals("SAD",mood);
-        }
+    @Test
+    public void givenMood_WhenBad_ShouldReturnBad() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyzeMood("I am in Sad mood");
+        Assert.assertEquals("SAD",mood);
+    }
+
+    @Test
+    public void givenMood_WhenHappy_ShouldReturnHappy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyzeMood("I am in Any mood");
+        Assert.assertEquals("HAPPY",mood);
+    }
 
 }
