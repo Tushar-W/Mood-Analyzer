88653ca8121d66ffce4c03a407d08b9c733fbdb4
Tushar-W <twankhede95@gmail.com>
2020-04-23 11:08:58 +0530

    ADDED:tested inform user if entered invalid mood

--- src/test/java/com/bl/demo/MoodAnalyzerTest.java
+++ src/test/java/com/bl/demo/MoodAnalyzerTest.java
@@ -1,5 +1,6 @@
 package com.bl.demo;
 
+import com.bl.demo.exception.MoodAnalysisException;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,21 +9,36 @@ public class MoodAnalyzerTest {
     @Test
     public void givenMood_WhenBad_ShouldReturnBad() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Sad mood");
-        String mood = moodAnalyzer.analyzeMood();
+        String mood = null;
+        try {
+            mood = moodAnalyzer.analyzeMood();
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
         Assert.assertEquals("SAD",mood);
     }
 
     @Test
     public void givenMood_WhenHappy_ShouldReturnHappy() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Happy mood");
-        String mood = moodAnalyzer.analyzeMood();
+        String mood = null;
+        try {
+            mood = moodAnalyzer.analyzeMood();
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
         Assert.assertEquals("HAPPY",mood);
     }
 
     @Test
     public void givenMood_WhenNull_ShouldReturnHappy() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
-        String mood = moodAnalyzer.analyzeMood();
-        Assert.assertEquals("HAPPY",mood);
+        String mood = null;
+        try {
+            mood = moodAnalyzer.analyzeMood();
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
     }
+
 }
