a09cbb775899ebc7c596ad70145cb3068779ab78
Tushar-W <twankhede95@gmail.com>
2020-04-23 10:04:13 +0530

    ADDED:tested sad mood

---
+++ src/test/java/com/bl/demo/MoodAnalyzerTest.java
@@ -0,0 +1,15 @@
+package com.bl.demo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+
+        @Test
+        public void givenMood_WhenBad_ShouldReturnBad() {
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+            String mood = moodAnalyzer.analyzeMood("I am in Sad mood");
+            Assert.assertEquals("SAD",mood);
+        }
+
+}
