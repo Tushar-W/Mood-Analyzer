8ec4d5ec30af677cc86c7f304b4dbfbcc89d45ba
Tushar-W <twankhede95@gmail.com>
2020-04-24 22:11:08 +0530

    MODIFIED:function for happy and sad mood

--- src/main/java/com/bl/demo/MoodAnalyzer.java
+++ src/main/java/com/bl/demo/MoodAnalyzer.java
@@ -3,14 +3,21 @@ package com.bl.demo;
 public class MoodAnalyzer {
     public String message;
 
+    public MoodAnalyzer() {
+    }
+
     public MoodAnalyzer(String message) {
         this.message = message;
     }
 
-    public String analyzeMood(){
+    public String analyzeMood(String  message){
         if (message.contains("I am in Sad mood"))
             return "SAD";
         else
             return "HAPPY";
+    }
+
+    public String analyzeMood() {
+        return this.analyzeMood(message);
     }
 }
--- src/test/java/com/bl/demo/MoodAnalyzerTest.java
+++ src/test/java/com/bl/demo/MoodAnalyzerTest.java
@@ -4,17 +4,32 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyzerTest {
+    MoodAnalyzer moodAnalyzer;
 
     @Test
-    public void givenMood_WhenBad_ShouldReturnBad() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Sad mood");
+    public void givenMood_WhenSad_ShouldReturnSad() {
+        moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyzeMood("I am in Sad mood");
+        Assert.assertEquals("SAD",mood);
+    }
+
+    @Test
+    public void givenMood_WhenAnyMood_ShouldReturnHappy() {
+        moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyzeMood("I am in Any mood");
+        Assert.assertEquals("HAPPY",mood);
+    }
+
+    @Test
+    public void givenMood_WhenSadPaasThroughConstructor_ShouldReturnSad() {
+        moodAnalyzer = new MoodAnalyzer("I am in Sad mood");
         String mood = moodAnalyzer.analyzeMood();
         Assert.assertEquals("SAD",mood);
     }
 
     @Test
-    public void givenMood_WhenHappy_ShouldReturnHappy() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Happy mood");
+    public void givenMood_WhenHappyPassThroughConsructor_ShouldReturnHappy() {
+        moodAnalyzer = new MoodAnalyzer("I am in Happy mood");
         String mood = moodAnalyzer.analyzeMood();
         Assert.assertEquals("HAPPY",mood);
     }
