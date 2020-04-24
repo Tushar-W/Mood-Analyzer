aee4d103fd4d51881013d5e0a87a9c19efcae1ac
Tushar-W <twankhede95@gmail.com>
2020-04-24 11:04:33 +0530

    ADDED:handle exception by creating custom exception

--- src/main/java/com/bl/demo/exception/MoodAnalysisException.java
+++ src/main/java/com/bl/demo/exception/MoodAnalysisException.java
@@ -1,8 +1,11 @@
 package com.bl.demo.exception;
 
 public class MoodAnalysisException extends Exception {
+    public enum Exception_Type {
+        ENTERED_NULL,ENTERED_EMPTY;
+    }
 
-    public MoodAnalysisException(String message) {
+    public MoodAnalysisException(Exception_Type type,String message) {
         super(message);
     }
 }
