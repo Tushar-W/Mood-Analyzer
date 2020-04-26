f79bfefc6d06fff7beeae7256052b9376e036128
Tushar-W <twankhede95@gmail.com>
2020-04-27 03:05:19 +0530

    ADDED:tested handle NO SUCH METHOD ERROR EXCEPTION

--- src/test/java/com/bl/demo/MoodAnalyzerTest.java
+++ src/test/java/com/bl/demo/MoodAnalyzerTest.java
@@ -93,7 +93,6 @@ public class MoodAnalyzerTest {
         Assert.assertEquals(new MoodAnalyzer("I am in Happy mood"),obj);
     }
 
-
     @Test
     public void givenMoodAnalyzerClassConstructor_WhenNotProper_ShouldReturnThrowException() {
         try {
@@ -102,6 +101,17 @@ public class MoodAnalyzerTest {
         } catch (MoodAnalysisException e) {
             Assert.assertEquals("NO SUCH CLASS ERROR",e.getMessage());
         }
+    }
+
+    @Test
+    public void givenMoodAnalyzerConstructorMethod_WhenNotProper_ShouldReturnThrowException()  {
+        try {
+            MoodAnalyzer obj = MoodAnalyzerFactory.createMoodAnalyzer("com.bl.demo.MoodAnalyzer",Integer.class,"I am in Happy mood");
+            Assert.assertEquals(new MoodAnalyzer("I am in Happy mood"),obj);
+        } catch (MoodAnalysisException e) {
+            Assert.assertEquals("NO SUCH METHOD ERROR",e.getMessage());
+        }
+
     }
 
 }
